94f7f08f9bc447e5d817f9b7ec4bd03f077ab9a1
Julien Silance <jusilanc@fu-c3-p6.s19.be>
2023-10-08 14:01:58 +0200

    Rename of config

--- include/Config.hpp
+++ include/Config.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Config.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hgeissle <hgeissle@student.s19.be>         +#+  +:+       +#+        */
+/*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:41:26 by hgeissle          #+#    #+#             */
-/*   Updated: 2023/10/06 17:03:46 by hgeissle         ###   ########.fr       */
+/*   Updated: 2023/10/08 14:00:35 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <vector>
 # include <map>
 # include <dirent.h>
+
 # include "Server.hpp"
 # include "Location.hpp"
 
