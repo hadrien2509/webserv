fb4e9e07ae5664af03e3bc5ba1ea767cc608bedc
hadrien <hadrien2509@gmail.com>
2023-10-14 01:37:11 +0200

    git

--- srcs/Run.cpp
+++ srcs/Run.cpp
@@ -6,7 +6,7 @@
 /*   By: hgeissle <hgeissle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 15:33:20 by hgeissle          #+#    #+#             */
-/*   Updated: 2023/10/14 00:34:02 by hgeissle         ###   ########.fr       */
+/*   Updated: 2023/10/14 01:32:25 by hgeissle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,22 +64,22 @@ void Config::run()
 				std::string httpResponse = response->get();
 				delete response;
 
-				send(client_socket, httpResponse.c_str(), httpResponse.size(), 0);
-				// try
-				// {
-				// /* code */
-				// 	std::string strFromCgi = cgiHandler(server->getCgiExtension(), server->getCgiPath(), request.getPath());
-				// 	send(client_socket, strFromCgi.c_str(), strFromCgi.size(), 0);
-				// }
-				// catch(const Cgi::CgiNotCgiException& e)
-				// {
-				// 	std::cerr << e.what() << '\n';
-				// 	send(client_socket, httpResponse.c_str(), httpResponse.size(), 0);
-				// }
-				// catch(const std::exception& e)
-				// {
-				// 	std::cerr << e.what() << '\n';
-				// }
+				// send(client_socket, httpResponse.c_str(), httpResponse.size(), 0);
+				try
+				{
+				/* code */
+					std::string strFromCgi = cgiHandler(server->getCgiExtension(), server->getCgiPath(), request.getPath());
+					send(client_socket, strFromCgi.c_str(), strFromCgi.size(), 0);
+				}
+				catch(const Cgi::CgiNotCgiException& e)
+				{
+					std::cerr << e.what() << '\n';
+					send(client_socket, httpResponse.c_str(), httpResponse.size(), 0);
+				}
+				catch(const std::exception& e)
+				{
+					std::cerr << e.what() << '\n';
+				}
             }
         }
 	}
