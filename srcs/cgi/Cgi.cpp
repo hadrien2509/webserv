b46b223455ca20f7b70f4a34d430d01044e2fd09
jsilance <juliensilance@hotmail.com>
2023-10-21 12:05:49 +0200

    importing previous cgi version

--- srcs/cgi/Cgi.cpp
+++ srcs/cgi/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 16:28:09 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/10/20 15:27:16 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/10/21 12:05:15 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -193,3 +193,68 @@ const std::string& Cgi::run()
 	}
 	return (_fromOut);
 }
+
+// const std::string Cgi::run()
+// {
+// 	pid_t pid;
+// 	int oldStdin;
+// 	(void)oldStdin;
+// 	int oldStdout;
+// 	(void)oldStdout;
+// 	int fdIn[2];
+// 	int fdOut[2];
+// 	int ret = 1;
+// 	char buffer[1024];
+	
+// 	// oldStdin = dup(STDIN_FILENO);
+// 	// oldStdout = dup(STDOUT_FILENO);
+
+// 	if (pipe(fdIn) == -1)
+// 	{
+// 		throw CgiPipeException();
+// 	}
+
+// 	if (pipe(fdOut) == -1)
+// 	{
+// 		close(fdIn[0]);
+// 		close(fdIn[1]);
+// 		throw CgiPipeException();
+// 	}
+
+// 	write(fdIn[1], _toIn.c_str(), _toIn.size());
+
+// 	pid = fork();
+
+// 	if (pid == -1)
+// 	{
+// 		throw CgiForkException();
+// 	}
+// 	else if (!pid)
+// 	{
+// 		// here for the execve
+		
+// 		close(fdIn[1]);
+// 		close(fdOut[0]);
+// 		dup2(fdIn[0], STDIN_FILENO);
+// 		dup2(fdOut[1], STDOUT_FILENO);
+// 		// execve(_path.c_str(), NULL, NULL); // need to add path form interpreter ex: python... and _env
+// 		std::cerr << "CGI exception: execve failed" << std::endl;
+// 		write(fdOut[1], "500 Internal Server Error\n", 26);
+// 	}
+// 	else
+// 	{
+// 		// here for the waitpid
+// 		close(fdIn[0]);
+// 		close(fdOut[1]);
+// 		dup2(fdOut[0], STDOUT_FILENO);
+// 		dup2(fdIn[1], STDIN_FILENO);
+// 		waitpid(-1, NULL, 0);
+
+// 		while (ret > 0)
+// 		{
+// 			ret = read(fdOut[0], buffer, 1024);
+// 			_fromOut += std::string(buffer, ret);
+// 		}
+// 	}
+// 	return (_fromOut);
+// }
